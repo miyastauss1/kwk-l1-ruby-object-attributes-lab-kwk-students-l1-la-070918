9614adab0939921932d70fc4f0e5740660745b4b
Miya Stauss <miyastauss1@gmail.com>
2018-07-12 22:31:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,38 +1,35 @@
-
-
-
-
-
-
-
-
-
-
-
 class Dog
-  def initialize(name,breed)
-  @name = name
-  @breed = breed
+ def initalize(name,breed)
+ @name = name
+ @breed = breed
 end
 
 def name
-    @name = "Fido"
+ @name = "Fido"
 end
 
 def name=(new_name)
-  @name=new_name
+ @name = new_name
 end
 
 def breed
-  @breed= "Fido"
+ @breed = "Beagle"
 end
 
 def breed=(new_breed)
-  @breed=new_breed
+ @breed = new_breed 
+end
+
 end
 
 
-end
+
+
+
+
+
+
+
 
 
 
