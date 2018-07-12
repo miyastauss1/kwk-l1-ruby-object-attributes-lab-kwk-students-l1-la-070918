399996abf2d881cc092887848148c1044d7e8ed9
Miya Stauss <miyastauss1@gmail.com>
2018-07-12 22:30:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,27 @@
 class Dog
-  
-def initialize(name,breed)
+  def initialize(name,breed)
   @name = name
   @breed = breed
 end
 
 def name
-    @name = name
+    @name = "Fido"
 end
 
-def breed = (new_breed)
-    @breed = new_breed
+def name=(new_name)
+  @name=new_name
+end
+
+def breed
+  @breed= "Fido"
+end
+
+def breed=(new_breed)
+  @breed=new_breed
 end
 
 
 end
+
+
+
